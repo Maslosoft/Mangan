99e5755935241b29c47b78ff3429ef3ae37ef470
Dariusz Górecki <darek.krk@gmail.com>
2010-12-24 18:52:01 +0100

    Updated DocBlocks

--- tests/unit/EMongoCriteriaTest.php
+++ tests/unit/EMongoCriteriaTest.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * EMongoCriteriaTest.php
+ *
+ * PHP version 5.2+
+ *
+ * @author		Dariusz Górecki <darek.krk@gmail.com>
+ * @author		Invenzzia Group, open-source division of CleverIT company http://www.invenzzia.org
+ * @copyright	2010 CleverIT http://www.cleverit.com.pl
+ * @license		http://www.yiiframework.com/license/ BSD license
+ * @version		1.3
+ * @category	ext
+ * @package		ext.YiiMongoDbSuite
+ *
+ */
 
 class EMongoCriteriaTest extends CTestCase
 {
