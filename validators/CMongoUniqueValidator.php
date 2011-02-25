904192a6d7be8108ee40174acc39e0acd78812b2
Florian Fackler <florian.fackler@mintao.com>
2011-02-25 21:08:25 +0100

    Fixed UniqueValidator error message

--- validators/CMongoUniqueValidator.php
+++ validators/CMongoUniqueValidator.php
@@ -18,28 +18,31 @@
  */
 class CMongoUniqueValidator extends CValidator
 {
-  /**
-   * @var boolean whether the attribute value can be null or empty. Defaults to true,
-   * meaning that if the attribute is empty, it is considered valid.
-   */
-  public $allowEmpty=true;
+	/**
+	 * @var boolean whether the attribute value can be null or empty. Defaults to true,
+	 * meaning that if the attribute is empty, it is considered valid.
+	 */
+	public $allowEmpty=true;
 
-  /**
-   * Validates the attribute of the object.
-   * If there is any error, the error message is added to the object.
-   * @param CModel $object the object being validated
-   * @param string $attribute the attribute being validated
-   */
-  protected function validateAttribute($object,$attribute)
-  {
-    $value=$object->$attribute;
-    if($this->allowEmpty && $this->isEmpty($value))
-      return;
-    $check = $object->find(array($attribute => $value));
-    if($check)
-    {
-      $message=$this->message!==null?$this->message:Yii::t('yii','{attribute} "{value}" has already been taken.');
-      $this->addError($object,$attribute,$message);
-    }
-  }
+	/**
+	 * Validates the attribute of the object.
+	 * If there is any error, the error message is added to the object.
+	 * @param CModel $object the object being validated
+	 * @param string $attribute the attribute being validated
+	 */
+	protected function validateAttribute($object,$attribute)
+	{
+		$value=$object->$attribute;
+		if($this->allowEmpty && $this->isEmpty($value))
+			return;
+		$check = $object->find(array($attribute => $value));
+		if($check)
+		{
+			$message=$this->message!==null?$this->message:Yii::t(
+				'yii','{attribute} "{value}" has already been taken.',
+				array('{value}' => $value)
+			);
+			$this->addError($object,$attribute,$message);
+		}
+	}
 }
