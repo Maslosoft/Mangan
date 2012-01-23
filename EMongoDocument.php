f73d5a8d4e77d1ba813b51d50b3ccf6c0fdb608e
Florian Fackler <florian.fackler@mintao.com>
2012-01-23 21:51:53 +0100

    Bugfix: Removed singleton pattern in model() method to be able to instanciate several models with different criteria

--- EMongoDocument.php
+++ EMongoDocument.php
@@ -1276,13 +1276,9 @@ abstract class EMongoDocument extends EMongoEmbeddedDocument {
 	*/
 	public static function model($className=__CLASS__)
 	{
-		if (isset(self::$_models[$className]))
-			return self::$_models[$className];
-		else {
-			$model=self::$_models[$className]=new $className(null);
-			$model->attachBehaviors($model->behaviors());
-			return $model;
-		}
+                $model=self::$_models[$className]=new $className(null);
+                $model->attachBehaviors($model->behaviors());
+                return $model;
 	}
 
 	/**
