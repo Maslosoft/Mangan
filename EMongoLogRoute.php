753f84effa3c168f3cc58cbdcd910a394bd176b4
isevi <ianare.sevi@digitick.com>
2012-05-25 16:22:42 +0200

    logroute : update comments, remove unused options

--- EMongoLogRoute.php
+++ EMongoLogRoute.php
@@ -1,75 +1,43 @@
 <?php
 
 /**
- * Auther aoyagikouhei
- *
- * 2011/08/01 ver 1.2
- * Add fsync, safe, timeout options
- *
- * 2011/07/09 ver 1.1
- * Add capped collection : Thank you joblo
- *
- * 2011/06/23 ver 1.0
- * First release
+ * @author aoyagikouhei (original author)
+ * @author ianaré sévi (merge into EMongoDB)
+ * 
+ * @license New BSD License
  *
  * Install
  * Extract the release file under protected/extensions
+ *
+ * Example, in config/main.php:
+ * 'log'=>array(
+ *		'class'=>'CLogRouter',
+ *		'routes'=>array(
+ *			array(
+ *				'class'=>'ext.EMongoDbLogRoute',
+ *				'levels'=>'trace, info, error, warning',
+ *				'categories' => 'system.*',
+ *				'collectionName' => 'yiilog',
+ *			),
+ *		),
+ * ),
  * 
- * In config/main.php:
-  'log'=>array(
-  'class'=>'CLogRouter',
-  'routes'=>array(
-  array(
-  'class'=>'ext.EMongoDbLogRoute',
-  'levels'=>'trace, info, error, warning',
-  'categories' => 'system.*',
-  ),
-  ),
-  ),
+ * Options:
+ * mongo					: mongo component name			: default mongodb
+ * collectionName			: collaction name				: default yiilog
+ * message					: message column name			: default message
+ * level					: level column name				: default level
+ * category					: category column name			: default category
+ * timestamp				: timestamp column name			: default timestamp
+ * timestampType			: float or date					: default float
+ * fsync					: fsync flag					: defalut false
+ * safe						: safe flag						: defalut false
+ * timeout					: timeout miliseconds			: defalut null i.e. MongoCursor::$timeout
  *
- * Options
- * connectionString        : host:port                      : defalut localhost:27017
- * dbName                  : database name                  : default test
- * collectionName          : collaction name                : default yiilog
- * message                 : message column name            : default message
- * level                   : level column name              : default level
- * category                : category column name           : default category
- * timestamp               : timestamp column name          : default timestamp
- * timestampType           : float or date                  : default float
- * collectionSize          : capped collection size         : default 10000
- * collectionMax           : capped collection max          : default 100
- * installCappedCollection : capped collection install flag : default false
- * fsync                   : fsync flag                     : defalut false
- * safe                    : safe flag                      : defalut false
- * timeout                 : timeout miliseconds            : defalut null i.e. MongoCursor::$timeout
- *
- * Example
-  'log'=>array(
-  'class'=>'CLogRouter',
-  'routes'=>array(
-  array(
-  'class'=>'ext.EMongoDbLogRoute',
-  'levels'=>'trace, info, error, warning',
-  'categories' => 'system.*',
-  'connectionString' => 'localhost:27017',
-  'dbName' => 'test',
-  'collectionName' => 'yiilog',
-  'message' => 'message',
-  'level' => 'level',
-  'category' => 'category',
-  'timestamp' => 'timestamp',
-  'timestampType' => 'float',
-  ,'collectionSize' => 10000
-  ,'collectionMax' => 100
-  ,'installCappedCollection' => true
-  ),
-  ),
-  ),
- *
- * Capped colection
- * 1. set installCappedCollection true in main.php.
- * 2. run application and loged
- * 3. remove installCappedCollection in main.php.
+ */
+
+/**
+ * EMongoLogRoute routes log messages to MongoDB.
  */
 class EMongoLogRoute extends CLogRoute
 {
@@ -79,26 +47,6 @@ class EMongoLogRoute extends CLogRoute
 	public $collectionName = 'yiilogs';
 
 	/**
-	 * @var string message column name
-	 */
-	public $message = 'message';
-
-	/**
-	 * @var string level column name
-	 */
-	public $level = 'level';
-
-	/**
-	 * @var string category column name
-	 */
-	public $category = 'category';
-
-	/**
-	 * @var string timestamp column name
-	 */
-	public $timestamp = 'timestamp';
-
-	/**
 	 * @var string timestamp type name float or date
 	 */
 	public $timestampType = 'float';
@@ -106,17 +54,17 @@ class EMongoLogRoute extends CLogRoute
 	/**
 	 * @var integer capped collection size
 	 */
-	public $collectionSize = 10000;
+	//public $collectionSize = 10000;
 
 	/**
 	 * @var integer capped collection max
 	 */
-	public $collectionMax = 100;
+	//public $collectionMax = 100;
 
 	/**
 	 * @var boolean capped collection install flag
 	 */
-	public $installCappedCollection = false;
+	//public $installCappedCollection = false;
 
 	/**
 	 * @var boolean forces the update to be synced to disk before returning success.
@@ -205,8 +153,14 @@ class EMongoLogRoute extends CLogRoute
 	}
 
 	/**
-	 * Saves log messages into mongodb.
-	 * @param array list of log messages
+	 * Processes log messages and sends them to specific destination.
+	 * @param array $logs list of messages.  Each array elements represents one message
+	 * with the following structure:
+	 * array(
+	 *   [0] => message (string)
+	 *   [1] => level (string)
+	 *   [2] => category (string)
+	 *   [3] => timestamp (float, obtained by microtime(true));
 	 */
 	protected function processLogs($logs)
 	{
@@ -216,7 +170,7 @@ class EMongoLogRoute extends CLogRoute
 					$this->message => $log[0],
 					$this->level => $log[1],
 					$this->category => $log[2],
-					$this->timestamp => 'date' === $this->timestampType ? new MongoDate(round($log[3])) : $log[3]
+					$this->timestamp => ($this->timestampType === 'date') ? new MongoDate(round($log[3])) : $log[3]
 					),
 				$this->_options
 			);
