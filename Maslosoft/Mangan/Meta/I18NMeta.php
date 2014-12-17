40cb9c670710f9726a828e63b4861bae6e1d4bf7
Peter Maselkowski <pmaselkowski@gmail.com>
2014-12-17 13:46:56 +0100

    * I18N

--- Maslosoft/Mangan/Meta/I18NMeta.php
+++ Maslosoft/Mangan/Meta/I18NMeta.php
@@ -8,6 +8,9 @@
 
 namespace Maslosoft\Mangan\Meta;
 
+use Maslosoft\Mangan\Decorators\I18NDecorator;
+use Maslosoft\Mangan\Helpers\PropertyMaker;
+
 /**
  * I18NMeta
  *
@@ -16,8 +19,43 @@ namespace Maslosoft\Mangan\Meta;
 class I18NMeta
 {
 
+	use \Maslosoft\Mangan\Traits\Access\GetSet;
+
 	public $enabled = false;
 	public $allowDefault = false;
 	public $allowAny = false;
+	private $_enabled = false;
+
+	/**
+	 * Parent metadata
+	 * @var DocumentPropertyMeta
+	 */
+	private $_meta = null;
+
+	public function __construct(DocumentPropertyMeta $meta)
+	{
+		PropertyMaker::defineProperty($this, 'enabled');
+		$this->_meta = $meta;
+	}
+
+	public function getEnabled()
+	{
+		return $this->_enabled;
+	}
+
+	public function setEnabled($enabled)
+	{
+		if ($enabled)
+		{
+			$this->_enabled = true;
+			$this->_meta->decorators[] = I18NDecorator::class;
+		}
+		else
+		{
+			$this->_enabled = true;
+			$key = array_search(I18NDecorator::class, $this->_meta->decorators);
+			unset($this->_meta->decorators[$key]);
+		}
+	}
 
 }
