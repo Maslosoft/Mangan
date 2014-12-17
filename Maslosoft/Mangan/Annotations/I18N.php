c2540a28c3f4c0835c004edeed09249cec6bdfc6
Peter Maselkowski <pmaselkowski@gmail.com>
2014-12-17 13:38:20 +0100

    + I18N annotation and meta

---
+++ Maslosoft/Mangan/Annotations/I18N.php
@@ -0,0 +1,32 @@
+<?php
+
+use Maslosoft\Addendum\Collections\MetaAnnotation;
+use Maslosoft\Mangan\Meta\I18NMeta;
+
+/**
+ * This annotation indicates internationallized fields
+ * @template I18N
+ * @Target('property')
+ * @author Piotr
+ */
+class I18N extends MetaAnnotation
+{
+
+	public $value = true;
+	public $allowDefault = false;
+	public $allowAny = false;
+
+	public function init()
+	{
+		if ($this->allowDefault && $this->allowAny)
+		{
+			throw new InvalidArgumentException(sprintf('Arguments "allowDefault" and "allowAny" for element "%s" in class "%s" cannot be both set true', $this->name, $this->_meta->type()->name));
+		}
+		$i18n = new I18NMeta();
+		$i18n->enabled = $this->value;
+		$i18n->allowDefault = $this->allowDefault;
+		$i18n->allowAny = $this->allowAny;
+		$this->_entity->i18n = $i18n;
+	}
+
+}
---
+++ Maslosoft/Mangan/Meta/I18NMeta.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Maslosoft\Mangan\Meta;
+
+/**
+ * I18NMeta
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class I18NMeta
+{
+
+	public $enabled = false;
+	public $allowDefault = false;
+	public $allowAny = false;
+
+}
