6cbd150af724843192b0ce27c4a316d521751982
Peter Maselkowski <pmaselkowski@gmail.com>
2015-03-04 13:25:07 +0100

    +* Safe annotation

--- src/Annotations/SafeValidatorAnnotation.php
+++ src/Annotations/SafeValidatorAnnotation.php
@@ -13,23 +13,24 @@
 
 namespace Maslosoft\Mangan\Annotations;
 
-use Maslosoft\Mangan\Base\ValidatorAnnotation;
-use Maslosoft\Mangan\Base\IBuiltInValidatorAnnotation;
+use Maslosoft\Mangan\Meta\ManganPropertyAnnotation;
 
 /**
- * NOTE: This class is automatically generated from Yii validator class.
- * This is not actual validator. For validator class @see CSafeValidator.
- */
-
-/**
- * CSafeValidator marks the associated attributes to be safe for massive assignments.
+ * Safe validator marks the associated attributes to be safe for massive assignments.
  *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @version $Id$
- * @package system.validators
- * @since 1.1
  */
-class SafeValidatorAnnotation extends ValidatorAnnotation implements IBuiltInValidatorAnnotation
+class SafeValidatorAnnotation extends ManganPropertyAnnotation
 {
+
+	/**
+	 * 
+	 * @var bool Whenever attribute is safe for mass assignment
+	 */
+	public $value = true;
+
+	public function init()
+	{
+		$this->_entity->safe = (bool) $this->value;
+	}
 
 }
