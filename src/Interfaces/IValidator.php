76773ab5aea6681bac9687518b7b8a7712fa7ff5
Peter Maselkowski <pmaselkowski@gmail.com>
2015-03-04 15:42:00 +0100

    +~ Validator interfaces

---
+++ src/Interfaces/IValidator.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Maslosoft\Mangan\Interfaces;
+
+/**
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+interface IValidator
+{
+	public function addError($message);
+	public function isValid();
+}
