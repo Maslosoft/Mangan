6e7a8ba9c2f87dd3466d11e241f24b5168091357
Peter Maselkowski <pmaselkowski@gmail.com>
2015-03-05 11:38:19 +0100

    * class headers

--- src/Base/IBuiltInValidatorAnnotation.php
+++ src/Base/IBuiltInValidatorAnnotation.php
@@ -1,4 +1,16 @@
 <?php
+
+/**
+ * This software package is licensed under New BSD license.
+ *
+ * @package maslosoft/mangan
+ * @licence New BSD
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @copyright Copyright (c) Others as mentioned in code
+ * @link http://maslosoft.com/mangan/
+ */
+
 namespace Maslosoft\Mangan\Base;
 /**
  * Interface to distinguish built-in validators from user validators
