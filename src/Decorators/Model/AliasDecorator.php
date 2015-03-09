b82e14f8dacf4b1637e850c989b555ed6856784e
Peter Maselkowski <pmaselkowski@gmail.com>
2015-03-09 16:42:06 +0100

    * headers

--- src/Decorators/Model/AliasDecorator.php
+++ src/Decorators/Model/AliasDecorator.php
@@ -1,15 +1,19 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This software package is licensed under New BSD license.
+ *
+ * @package maslosoft/mangan
+ * @licence New BSD
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @copyright Copyright (c) Others as mentioned in code
+ * @link http://maslosoft.com/mangan/
  */
 
 namespace Maslosoft\Mangan\Decorators\Model;
 
 use Maslosoft\Addendum\Interfaces\IAnnotated;
-use Maslosoft\Mangan\EmbeddedDocument;
 use Maslosoft\Mangan\Meta\DocumentPropertyMeta;
 use Maslosoft\Mangan\Meta\DocumentTypeMeta;
 use Maslosoft\Mangan\Meta\ManganMeta;
