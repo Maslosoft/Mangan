e6051c48ed2b9383b97aae2387328bfa11c5de2c
Peter Maselkowski <pmaselkowski@gmail.com>
2015-05-04 13:18:00 +0200

    + Alias PassThrough sanitizer #32

---
+++ src/Sanitizers/None.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This software package is licensed under AGPL or Commercial license.
+ *
+ * @package maslosoft/mangan
+ * @licence AGPL or Commercial
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @copyright Copyright (c) Others as mentioned in code
+ * @link http://maslosoft.com/mangan/
+ */
+
+namespace Maslosoft\Mangan\Sanitizers;
+
+/**
+ * None sanitizer, alias to PassThrough.
+ * Empty sanitizer, does not change anything
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class None extends PassThrough
+{
+
+}
