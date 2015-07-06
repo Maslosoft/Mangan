41218a2ceb2ffc9d68924d9f878e55d6127c63e8
Peter Maselkowski <pmaselkowski@gmail.com>
2015-07-06 21:37:38 +0200

    * Made possible var_export of ManganMeta

--- src/Meta/I18NMeta.php
+++ src/Meta/I18NMeta.php
@@ -13,9 +13,6 @@
 
 namespace Maslosoft\Mangan\Meta;
 
-use Maslosoft\Mangan\Decorators\Property\I18NDecorator;
-use Maslosoft\Mangan\Helpers\PropertyMaker;
-
 /**
  * I18NMeta
  *
@@ -24,43 +21,8 @@ use Maslosoft\Mangan\Helpers\PropertyMaker;
 class I18NMeta
 {
 
-	use \Maslosoft\Mangan\Traits\Access\GetSet;
-
 	public $enabled = false;
 	public $allowDefault = false;
 	public $allowAny = false;
-	private $_enabled = false;
-
-	/**
-	 * Parent metadata
-	 * @var DocumentPropertyMeta
-	 */
-	private $_meta = null;
-
-	public function __construct(DocumentPropertyMeta $meta)
-	{
-		PropertyMaker::defineProperty($this, 'enabled');
-		$this->_meta = $meta;
-	}
-
-	public function getEnabled()
-	{
-		return $this->_enabled;
-	}
-
-	public function setEnabled($enabled)
-	{
-		if ($enabled)
-		{
-			$this->_enabled = true;
-			$this->_meta->decorators[] = I18NDecorator::class;
-		}
-		else
-		{
-			$this->_enabled = true;
-			$key = array_search(I18NDecorator::class, $this->_meta->decorators);
-			unset($this->_meta->decorators[$key]);
-		}
-	}
 
 }
