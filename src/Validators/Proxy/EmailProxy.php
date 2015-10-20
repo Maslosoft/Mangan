8bea35e6295aeb7c719c1c63818b5e10bf433648
Peter Maselkowski <pmaselkowski@gmail.com>
2015-10-20 19:28:15 +0200

    + Validator proxies

---
+++ src/Validators/Proxy/EmailProxy.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Maslosoft\Mangan\Validators\Proxy;
+
+/**
+ * EmailProxy
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class EmailProxy
+{
+	
+}
