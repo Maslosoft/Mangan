8968e2c6337987ddbc9c166444e8df7464aa729d
Peter Maselkowski <pmaselkowski@gmail.com>
2015-12-29 11:01:21 +0100

    + JsonString transformer

--- src/Transformers/JsonString.php
+++ src/Transformers/JsonString.php
@@ -13,12 +13,42 @@
 
 namespace Maslosoft\Mangan\Transformers;
 
+use Maslosoft\Addendum\Interfaces\AnnotatedInterface;
+use Maslosoft\Mangan\Exceptions\TransformatorException;
+use Maslosoft\Mangan\Interfaces\Transformators\TransformatorInterface;
+
 /**
  * JsonString
  *
  * @author Piotr Maselkowski <pmaselkowski at gmail.com>
  */
-class JsonString extends JsonArray
+class JsonString implements TransformatorInterface
 {
+
+	/**
+	 * Returns the given object as an associative array
+	 * @param AnnotatedInterface|object $model
+	 * @param string[] $fields Fields to transform
+	 * @param int $options json_encode options
+	 * @return array an associative array of the contents of this object
+	 */
+	public static function fromModel(AnnotatedInterface $model, $fields = [], $options = null)
+	{
+		return json_encode(JsonArray::fromModel($model, $fields), $options);
+	}
+
+	/**
+	 * Create document from array
+	 *
+	 * @param mixed[] $data
+	 * @param string|object $className
+	 * @param AnnotatedInterface $instance
+	 * @return AnnotatedInterface
+	 * @throws TransformatorException
+	 */
+	public static function toModel($data, $className = null, AnnotatedInterface $instance = null)
+	{
+		return JsonArray::toModel(json_decode($data, true), $className, $instance);
+	}
 
 }
