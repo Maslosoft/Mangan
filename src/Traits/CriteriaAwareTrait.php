c2b4d387280a25f00726863b5c35e07c0877187e
Peter Maselkowski <pmaselkowski@gmail.com>
2016-08-04 09:39:11 +0200

    + Basic implementation of Criteria Aware Interface

---
+++ src/Traits/CriteriaAwareTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Maslosoft\Mangan\Traits;
+
+use Maslosoft\Mangan\Interfaces\CriteriaInterface;
+
+/**
+ * The most simple Criteria Aware Interface implementation.
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+trait CriteriaAwareTrait
+{
+
+	/**
+	 * Criteria instance holder.
+	 * @var CriteriaInterface
+	 */
+	private $criteria = null;
+
+	/**
+	 * Get criteria
+	 * @return CriteriaInterface
+	 */
+	public function getCriteria()
+	{
+		return $this->criteria;
+	}
+
+	/**
+	 * Set criteria
+	 * @param CriteriaInterface $criteria
+	 * @return static
+	 */
+	public function setCriteria(CriteriaInterface $criteria)
+	{
+		$this->criteria = $criteria;
+		return $this;
+	}
+
+}
