e1e785220765d35f227f957b0d90e4c20ee933b1
Peter Maselkowski <pmaselkowski@gmail.com>
2016-08-04 08:55:40 +0200

    >> Splitting common data provider parts, part 2

--- src/Traits/DataProvider/ConfigureTrait.php
+++ src/Traits/DataProvider/ConfigureTrait.php
@@ -19,19 +19,48 @@ use Maslosoft\Mangan\Meta\ManganMeta;
 trait ConfigureTrait
 {
 
-	protected function configure($config)
+	protected function configure($modelClass, $config)
 	{
+		if (is_string($modelClass))
+		{
+			$this->model = new $modelClass;
+		}
+		elseif (is_object($modelClass))
+		{
+			$this->model = $modelClass;
+		}
+		else
+		{
+			throw new ManganException('Invalid model type for ' . static::class);
+		}
+
+
+		// Set criteria from model
+		$criteria = $this->getCriteria();
+		if ($criteria instanceof MergeableInterface)
+		{
+			// NOTE: WithCriteria and CriteriaAware have just slightly different method names
+			if ($this->model instanceof WithCriteriaInterface)
+			{
+				$criteria->mergeWith($this->model->getDbCriteria());
+			}
+			elseif ($this->model instanceof CriteriaAwareInterface)
+			{
+				$criteria->mergeWith($this->model->getCriteria());
+			}
+		}
+
 		// Merge criteria from configuration
 		if (isset($config['criteria']))
 		{
-			$this->getCriteria()->mergeWith($config['criteria']);
+			$criteria->mergeWith($config['criteria']);
 			unset($config['criteria']);
 		}
 
 		// Merge limit from configuration
 		if (isset($config['limit']) && $config['limit'] > 0)
 		{
-			$this->getCriteria()->setLimit($config['limit']);
+			$criteria->setLimit($config['limit']);
 			unset($config['limit']);
 		}
 
@@ -41,16 +70,16 @@ trait ConfigureTrait
 			// Apply default sorting if criteria does not have sort configured
 			if (isset($config['sort']['defaultOrder']) && empty($this->getCriteria()->getSort()))
 			{
-				$this->getCriteria()->setSort($config['sort']['defaultOrder']);
+				$criteria->setSort($config['sort']['defaultOrder']);
 			}
 			unset($config['sort']);
 		}
 
-		if (!$this->getCriteria()->getSelect())
+		if (!$criteria->getSelect())
 		{
 			$fields = array_keys(ManganMeta::create($this->model)->fields());
 			$selected = array_fill_keys($fields, true);
-			$this->getCriteria()->setSelect($selected);
+			$criteria->setSelect($selected);
 		}
 
 		foreach ($config as $key => $value)
