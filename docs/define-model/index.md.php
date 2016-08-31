922c357ce3e1236a176f587da48f5b3b0f4d10b2
Peter Maselkowski <pmaselkowski@gmail.com>
2016-08-31 14:35:43 +0200

    * docs

---
+++ docs/define-model/index.md.php
@@ -0,0 +1,17 @@
+<?php
+
+use Maslosoft\Ilmatar\Widgets\Form\ActiveForm;
+use Maslosoft\Ilmatar\Components\Controller;
+?>
+<?php
+/* @var $this Controller */
+/* @var $form ActiveForm */
+?>
+<template>docs</template>
+<title>Define Model</title>
+#Define Model
+
+All model properties can be defined with [annotations](../annotations/) implemented by [addendum project](/addendum/).
+This concept was chosen to allow storing arbitrary PHP object without the need of extending
+from any base class or implementing any methods.
+
