fb864b05fa97629c80c099d0b844d21c821a6821
Peter Maselkowski <pmaselkowski@gmail.com>
2017-03-27 20:04:05 +0200

    * docs

--- docs/index.md.php
+++ docs/index.md.php
@@ -10,3 +10,48 @@ use Maslosoft\Ilmatar\Components\Controller;
 
 <template>docs</template>
 <title>Documentation</title>
+
+# Documentation
+
+Mangan goal is to allow storing PHP objects in MongoDB. These can be
+composed in more complex structures, be either embedding, referencing or relations.
+It is generally ready to use out of a package. However at least some minimal
+configuration is required, for instance database name.
+
+### Installation
+
+Use composer to install mangan:
+
+```
+composer require maslosoft/mangan
+```
+
+### Configuration
+
+It is recommended to use [EmbeDi](/embedi/) to configure Mangan. This allow
+separation configuration from actual usage. Minimum configuration required
+is database name, using property `dbName`. Default configuration ID is `mongodb`.
+
+*Use statements omitted*
+```
+$config = [
+	'mongodb' => [
+		'class' => Mangan::class,
+		// Database name
+		'dbName' => 'quick-start',
+	]
+];
+EmbeDi::fly()->addAdapter(new ArrayAdapter($config));
+```
+
+Now, assuming that we have some models ready, Mangan is ready to do it's duty.
+When using built-in base documents classes, Active Document (derived from active record)
+pattern can be used, for instance:
+
+```
+$plant = new Plant;
+$plant->name = 'Grass';
+$plant->save();
+```
+
+Check [this repository for working example of Mangan](https://github.com/MaslosoftGuides/mangan.quick-start)
