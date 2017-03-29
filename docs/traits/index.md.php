79b3f6d56f57713e2c584f38c066e5ec68080ac1
Peter Maselkowski <pmaselkowski@gmail.com>
2017-03-29 12:49:44 +0200

    * docs

--- docs/traits/index.md.php
+++ docs/traits/index.md.php
@@ -3,9 +3,7 @@
 use Maslosoft\Ilmatar\Components\Controller;
 use Maslosoft\Ilmatar\Widgets\Form\ActiveForm;
 use Maslosoft\Mangan\Traits\I18NAbleTrait;
-use Maslosoft\Zamm\DocBlock;
-use Maslosoft\Zamm\Iterator;
-use Maslosoft\Zamm\ShortNamer;
+use Maslosoft\Zamm\Widgets\NamespaceDocs;
 ?>
 <?php
 
@@ -16,13 +14,6 @@ use Maslosoft\Zamm\ShortNamer;
 <title>Traits</title>
 # Traits
 
-Mangan comes with various trait's for easier implementation of common cases
+[Mangan](/mangan/) comes with various trait's for easier implementation of common cases
 
-<?php
-
-foreach (Iterator::ns(I18NAbleTrait::class) as $class):
-	$header = '### ' . (new ShortNamer($class)) . PHP_EOL;
-	echo $header;
-	echo new DocBlock($class);
-endforeach;
-?>
+<?= new NamespaceDocs(I18NAbleTrait::class); ?>
