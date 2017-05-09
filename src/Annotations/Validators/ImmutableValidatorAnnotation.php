9ccf0689f62d2800ce3b2a6f31c24158b0a90a95
Peter Maselkowski <pmaselkowski@gmail.com>
2017-05-09 14:23:40 +0200

    + Proxy and Annotation for Immutable Validator #80

---
+++ src/Annotations/Validators/ImmutableValidatorAnnotation.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * This software package is licensed under AGPL or Commercial license.
+ *
+ * @package maslosoft/mangan
+ * @licence AGPL or Commercial
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @copyright Copyright (c) Others as mentioned in code
+ * @link https://maslosoft.com/mangan/
+ */
+
+namespace Maslosoft\Mangan\Annotations\Validators;
+
+use Maslosoft\Addendum\Helpers\ParamsExpander;
+use Maslosoft\Mangan\Meta\ValidatorMeta;
+use Maslosoft\Mangan\Validators\Proxy\ImmutableProxy;
+use Maslosoft\Mangan\Validators\Traits\AllowEmpty;
+use Maslosoft\Mangan\Validators\Traits\When;
+
+/**
+ * UniqueValidator
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class UniqueValidatorAnnotation extends ValidatorAnnotation
+{
+
+	use AllowEmpty,
+	  When;
+
+	/**
+	 * Against which attribute to validate, defaults to current
+	 * @var string
+	 */
+	public $against = '';
+
+	/**
+	 * @var string the ActiveRecord class name that should be used to
+	 * look for the attribute value being validated. Defaults to null, meaning using
+	 * the class of the object currently being validated.
+	 * You may use path alias to reference a class name here.
+	 * @see attributeName
+	 * @since 1.0.8
+	 */
+	public $className;
+
+	/**
+	 * @var string the ActiveRecord class attribute name that should be
+	 * used to look for the attribute value being validated. Defaults to null,
+	 * meaning using the name of the attribute being validated.
+	 * @see className
+	 * @since 1.0.8
+	 */
+	public $attributeName;
+
+	/**
+	 * @var array additional query criteria. This will be combined with the condition
+	 * that checks if the attribute value exists in the corresponding table column.
+	 * This array will be used to instantiate a {@link Criteria} object.
+	 * @since 1.0.8
+	 */
+	public $criteria = [];
+
+	/**
+	 * @var string the user-defined error message. The placeholders "{attribute}" and "{value}"
+	 * are recognized, which will be replaced with the actual attribute name and value, respectively.
+	 */
+	public $message;
+
+	public function init()
+	{
+		$this->proxy = ImmutableProxy::class;
+		$this->getEntity()->validators[] = new ValidatorMeta(ParamsExpander::expand($this, [
+					'allowEmpty',
+					'when',
+					'against',
+					'className',
+					'attributeName',
+					'criteria',
+					'message',
+					'skipOnError',
+					'on',
+					'safe',
+					'enableClientValidation',
+					'except',
+					'proxy'
+		]));
+	}
+
+}
---
+++ src/Validators/Proxy/ImmutableProxy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Maslosoft\Mangan\Validators\Proxy;
+
+/**
+ * ImmutableProxy
+ *
+ * @author Piotr Maselkowski <pmaselkowski at gmail.com>
+ */
+class ImmutableProxy
+{
+	
+}
