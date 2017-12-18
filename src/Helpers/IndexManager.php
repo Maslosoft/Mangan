560abd5646f328f8957b4e0fa702918a80678971
Peter Maselkowski <office@maslosoft.com>
2017-12-18 08:31:14 +0100

    + License headers

--- src/Helpers/IndexManager.php
+++ src/Helpers/IndexManager.php
@@ -1,9 +1,14 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: peter
- * Date: 17.12.17
- * Time: 14:36
+ * This software package is licensed under AGPL or Commercial license.
+ *
+ * @package maslosoft/mangan
+ * @licence AGPL or Commercial
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @copyright Copyright (c) Others as mentioned in code
+ * @link https://maslosoft.com/mangan/
  */
 
 namespace Maslosoft\Mangan\Helpers;
--- src/Validators/BuiltIn/CountValidator.php
+++ src/Validators/BuiltIn/CountValidator.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * This software package is licensed under AGPL or Commercial license.
+ *
+ * @package maslosoft/mangan
+ * @licence AGPL or Commercial
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @copyright Copyright (c) Others as mentioned in code
+ * @link https://maslosoft.com/mangan/
+ */
+
 namespace Maslosoft\Mangan\Validators\BuiltIn;
 
 use Countable;
