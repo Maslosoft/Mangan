efdea7f6426f8a273b56b7202e8bde46a3aa8297
Peter Maselkowski <office@maslosoft.com>
2017-12-18 08:31:14 +0100

    + License headers

--- src/Helpers/AnnotationDefaults.php
+++ src/Helpers/AnnotationDefaults.php
@@ -1,9 +1,14 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: peter
- * Date: 21.11.17
- * Time: 13:15
+ * This software package is licensed under AGPL or Commercial license.
+ *
+ * @package maslosoft/mangan
+ * @licence AGPL or Commercial
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @copyright Copyright (c) Others as mentioned in code
+ * @link https://maslosoft.com/mangan/
  */
 
 namespace Maslosoft\Mangan\Helpers;
--- src/Traits/AvailableCommands.php
+++ src/Traits/AvailableCommands.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * This software package is licensed under New BSD license.
+ * This software package is licensed under AGPL or Commercial license.
  *
  * @package maslosoft/mangan
- * @licence New BSD
+ * @licence AGPL or Commercial
  * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
  * @copyright Copyright (c) Maslosoft
  * @copyright Copyright (c) Others as mentioned in code
- * @link http://maslosoft.com/mangan/
+ * @link https://maslosoft.com/mangan/
  */
 
 namespace Maslosoft\Mangan\Traits;
