d44dfeff7d1bdb3637c6ff9a1835cb3b9fafbcf4
Peter Maselkowski <office@maslosoft.com>
2018-01-23 12:15:12 +0100

    * Meta

--- src/Exceptions/PkException.php
+++ src/Exceptions/PkException.php
@@ -1,9 +1,14 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: peter
- * Date: 23.01.18
- * Time: 10:45
+ * This software package is licensed under AGPL or Commercial license.
+ *
+ * @package maslosoft/mangan
+ * @licence AGPL or Commercial
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @copyright Copyright (c) Others as mentioned in code
+ * @link https://maslosoft.com/mangan/
  */
 
 namespace Maslosoft\Mangan\Exceptions;
--- tests/extensions/ModelComparator.php
+++ tests/extensions/ModelComparator.php
@@ -1,9 +1,14 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This software package is licensed under AGPL or Commercial license.
+ *
+ * @package maslosoft/mangan
+ * @licence AGPL or Commercial
+ * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
+ * @copyright Copyright (c) Maslosoft
+ * @copyright Copyright (c) Others as mentioned in code
+ * @link https://maslosoft.com/mangan/
  */
 
 namespace Maslosoft\ManganTest\Extensions;
