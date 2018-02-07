fa66ffb8b767e55a4cef8464ca9cc2d7fd2db384
Peter Maselkowski <office@maslosoft.com>
2018-02-07 21:30:51 +0100

    + Homogeneous annotation (alias to mistyped Homogenous)

---
+++ src/Annotations/HomogeneousAnnotation.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peter
+ * Date: 07.02.18
+ * Time: 21:28
+ */
+
+namespace Maslosoft\Mangan\Annotations;
+
+
+class HomogeneousAnnotation extends HomogenousAnnotation
+{
+
+}
