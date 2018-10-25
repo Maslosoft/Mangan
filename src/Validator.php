202b0339a015a5c0a4b7904e592cbd5b7596e5a3
Peter Maselkowski <office@maslosoft.com>
2018-10-25 13:01:25 +0200

    * Don't validate non-persistent fields when saving >> Refactored conditions into `shouldValidate` and other method

--- src/Validator.php
+++ src/Validator.php
@@ -3,12 +3,12 @@
 /**
  * This software package is licensed under AGPL or Commercial license.
  *
- * @package maslosoft/mangan
- * @licence AGPL or Commercial
+ * @package   maslosoft/mangan
+ * @licence   AGPL or Commercial
  * @copyright Copyright (c) Piotr Masełkowski <pmaselkowski@gmail.com>
  * @copyright Copyright (c) Maslosoft
  * @copyright Copyright (c) Others as mentioned in code
- * @link https://maslosoft.com/mangan/
+ * @link      https://maslosoft.com/mangan/
  */
 
 namespace Maslosoft\Mangan;
@@ -16,9 +16,13 @@ namespace Maslosoft\Mangan;
 use InvalidArgumentException;
 use Maslosoft\Addendum\Interfaces\AnnotatedInterface;
 use Maslosoft\Mangan\Helpers\Validator\Factory;
+use Maslosoft\Mangan\Interfaces\EntityManagerInterface;
 use Maslosoft\Mangan\Interfaces\OwneredInterface;
 use Maslosoft\Mangan\Interfaces\ValidatableInterface;
+use Maslosoft\Mangan\Meta\DocumentPropertyMeta;
+use Maslosoft\Mangan\Meta\DocumentTypeMeta;
 use Maslosoft\Mangan\Meta\ManganMeta;
+use Maslosoft\Mangan\Meta\ValidatorMeta;
 
 /**
  * Validator
@@ -72,14 +76,14 @@ class Validator implements ValidatableInterface
 			$fields = array_keys($this->meta->fields());
 		}
 
+		$meta = $this->meta->type();
 		// Model validators
-		$typeValidators = $this->meta->type()->validators;
-		if (!empty($typeValidators))
+		if ($this->hasValidators($meta))
 		{
-			$typeName = $this->meta->type()->name;
+			$typeName = $meta->name;
 			// Reset errors
 			$this->errors[$typeName] = [];
-			$valid[] = (int)$this->validateEntity($typeName, $typeValidators);
+			$valid[] = (int)$this->validateField($typeName, $meta->validators);
 		}
 
 		foreach ($fields as $name)
@@ -92,54 +96,42 @@ class Validator implements ValidatableInterface
 			// Check if meta for field exists
 			assert(!empty($fieldMeta), new InvalidArgumentException(sprintf("Unknown field `%s` in model `%s`", $name, get_class($this->model))));
 
-			// Validate field if has any validator
-			if (!empty($fieldMeta->validators))
+			// Validate if it is applicable
+			if ($this->hasValidators($fieldMeta))
 			{
-				$valid[] = (int)$this->validateEntity($name, $fieldMeta->validators);
+				$valid[] = (int)$this->validateField($name, $fieldMeta->validators);
 			}
 
 			// Validate sub documents
-			// Skip fields that are not updatable
-			if ($fieldMeta->owned && $fieldMeta->updatable)
+			if ($this->haveSubObjects($fieldMeta) && $this->shouldValidate($this->model, $fieldMeta))
 			{
 				if (is_array($this->model->$name))
 				{
 					// Handle arrays of documents
 					foreach ($this->model->$name as $fieldIndex => $model)
 					{
-						// Ensure owner, as validation might rely on it
-						if($model instanceof OwneredInterface)
-						{
-							$model->setOwner($this->model);
-						}
-						$validator = new Validator($model);
-						$isValid = $validator->validate();
-						$valid[] = (int)$isValid;
+						$errors = [];
+						$valid[] = (int)$isValid = $this->validateEntity($model, $errors);
 						if (!$isValid)
 						{
 							$errors = [
 								$name => [
-									$fieldIndex => $validator->getErrors()
+									$fieldIndex => $errors
 								]
 							];
 							$this->setErrors($errors);
 						}
 					}
-				} elseif (!empty($this->model->$name))
+				}
+				elseif (!empty($this->model->$name))
 				{
-					// Ensure owner, as validation might rely on it
-					if($this->model->$name instanceof OwneredInterface)
-					{
-						$this->model->$name->setOwner($this->model);
-					}
-					// Handle single documents
-					$validator = new Validator($this->model->$name);
-					$isValid = $validator->validate();
-					$valid[] = (int)$isValid;
+					$model = $this->model->$name;
+					$errors = [];
+					$valid[] = (int)$isValid = $this->validateEntity($model, $errors);
 					if (!$isValid)
 					{
 						$errors = [
-							$name => $validator->getErrors()
+							$name => $errors
 						];
 						$this->setErrors($errors);
 					}
@@ -150,14 +142,98 @@ class Validator implements ValidatableInterface
 		$areAllValid = count($valid) === array_sum($valid);
 
 		// For easier debug
-		if($areAllValid)
+		if ($areAllValid)
 		{
 			return true;
 		}
 		return false;
 	}
 
-	private function validateEntity($name, $validators)
+	/**
+	 * Whether field has any validators
+	 * @param DocumentTypeMeta|DocumentPropertyMeta $meta
+	 * @return bool
+	 */
+	private function hasValidators($meta)
+	{
+		assert($meta instanceof DocumentTypeMeta || $meta instanceof DocumentPropertyMeta);
+		return !empty($meta->validators);
+	}
+
+	/**
+	 * Check whether field has sub object or sub objects
+	 * @param DocumentPropertyMeta $fieldMeta
+	 * @return bool
+	 */
+	private function haveSubObjects(DocumentPropertyMeta $fieldMeta)
+	{
+		return $fieldMeta->owned;
+	}
+
+	/**
+	 * Check whether field should be validated.
+	 *
+	 * @param AnnotatedInterface   $model
+	 * @param DocumentPropertyMeta $fieldMeta
+	 * @return bool
+	 */
+	private function shouldValidate(AnnotatedInterface $model, DocumentPropertyMeta $fieldMeta)
+	{
+		// Skip fields that are owned...
+		if ($fieldMeta->owned)
+		{
+			// ... and not updatable
+			if (!$fieldMeta->updatable)
+			{
+				return false;
+			}
+		}
+
+		// Check if should validate when saving and removing...
+		if (AspectManager::hasAspect($model, EntityManagerInterface::AspectSaving) || AspectManager::hasAspect($model, EntityManagerInterface::AspectRemoving))
+		{
+			// ... when field is not persistent.
+			// This is to speed up validation and prevent false
+			// positive validations.
+			if (!$fieldMeta->persistent)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Validate single sub object entity. The `$errors`
+	 * parameter will contain error messages passed by
+	 * reference from this method.
+	 *
+	 * @param AnnotatedInterface $model
+	 * @param string[]           $errors
+	 * @return bool
+	 */
+	private function validateEntity(AnnotatedInterface $model, &$errors = [])
+	{
+		// Ensure owner, as validation might rely on it
+		if ($model instanceof OwneredInterface)
+		{
+			$model->setOwner($this->model);
+		}
+		assert($model instanceof AnnotatedInterface);
+		// Handle single documents
+		$validator = new Validator($model);
+		$isValid = $validator->validate();
+		$errors = $validator->getErrors();
+		return $isValid;
+	}
+
+	/**
+	 * Validate single, simple field
+	 * @param string          $name
+	 * @param ValidatorMeta[] $validators
+	 * @return bool
+	 */
+	private function validateField($name, $validators)
 	{
 		$valid = [];
 		foreach ($validators as $validatorMeta)
@@ -204,7 +280,8 @@ class Validator implements ValidatableInterface
 			if ($validator->isValid($this->model, $name))
 			{
 				$valid[] = true;
-			} else
+			}
+			else
 			{
 				$valid[] = false;
 				$this->errors[$name] = array_merge($this->errors[$name], $validator->getErrors());
