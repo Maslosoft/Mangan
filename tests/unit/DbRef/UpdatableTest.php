49a311a18bb52428c17f06d39df8fc59aa195c21
Peter Maselkowski <office@maslosoft.com>
2022-02-22 19:39:59 +0100

    ! Fixed test

--- tests/unit/DbRef/UpdatableTest.php
+++ tests/unit/DbRef/UpdatableTest.php
@@ -5,6 +5,7 @@ namespace DbRef;
 use Codeception\Test\Unit;
 use Maslosoft\Mangan\EntityManager;
 use Maslosoft\Mangan\Finder;
+use Maslosoft\Mangan\Meta\ManganMeta;
 use Maslosoft\ManganTest\Models\DbRef\ModelWithNotUpdatableDbRef;
 use Maslosoft\ManganTest\Models\DbRef\ModelWithNotUpdatableDbRefShortNotation;
 use Maslosoft\ManganTest\Models\DbRef\ModelWithNotUpdatableDbRefShortNotation2;
@@ -14,6 +15,7 @@ use Maslosoft\ManganTest\Models\Plain\SimplePlainDbRef;
 use MongoId;
 use UnexpectedValueException;
 use UnitTester;
+use function codecept_debug;
 
 class UpdatableTest extends Unit
 {
@@ -79,8 +81,12 @@ class UpdatableTest extends Unit
 			$this->markTestSkipped("PHP option `assert.exception` must be enabled for this test");
 		}
 		$model = new ModelWithNotUpdatableDbRefWrongAnnotationValue();
+
 		$this->expectException(UnexpectedValueException::class);
-		$this->subTestIfWillNoUpdateRefObject($model);
+		$meta = ManganMeta::create($model);
+		codecept_debug($meta->field('stats')->updatable);
+		codecept_debug('By default DbRefMeta updates refs, so check if will update');
+		$this->subTestIfWillUpdateRefObject($model);
 	}
 
 
@@ -104,6 +110,28 @@ class UpdatableTest extends Unit
 
 		$found = $statsFinder->findByPk($statsId);
 		$this->assertSame('one', $found->name);
+	}
+
+	private function subTestIfWillUpdateRefObject($model)
+	{
+		$stats = new SimplePlainDbRef();
+		$stats->name = 'one';
+		$statsId = $stats->_id = new MongoId;
+		$saved = (new EntityManager($stats))->save();
+		$this->assertTrue($saved);
+
+		$model->stats = $stats;
+
+		$statsFinder = new Finder($stats);
+		$found = $statsFinder->findByPk($statsId);
+		$this->assertSame('one', $found->name);
+
+		$model->stats->name = 'two';
+		$saved2 = (new EntityManager($model))->save();
+		$this->assertTrue($saved2);
+
+		$found = $statsFinder->findByPk($statsId);
+		$this->assertSame('two', $found->name);
 	}
 
 	private function subTestIfWillNoUpdateRefArrayObject($model)
