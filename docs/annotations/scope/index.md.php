349471187e60035a5869852e17d29cc35fe6838e
Peter Maselkowski <office@maslosoft.com>
2022-09-13 17:03:24 +0200

    + scope docs

---
+++ docs/annotations/scope/index.md.php
@@ -0,0 +1,13 @@
+<?php
+
+use Maslosoft\Mangan\Annotations\ScopeAnnotation;
+use Maslosoft\Zamm\DocBlock;
+?>
+<?php
+$doc = new DocBlock(ScopeAnnotation::class);
+?>
+<title>Scope</title>
+
+#Scope Annotation
+
+<?= $doc; ?>
