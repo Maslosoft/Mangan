01a3822541bbad93bcc299e8d709319399350fb6
Peter Maselkowski <office@maslosoft.com>
2023-04-12 17:10:39 +0200

    >> Modernize

--- tests/unit/Helpers/IndexManagerTest.php
+++ tests/unit/Helpers/IndexManagerTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Helpers;
 
+use Codeception\Test\Unit;
 use Maslosoft\Mangan\Command;
 use Maslosoft\Mangan\Finder;
 use Maslosoft\Mangan\Helpers\CollectionNamer;
@@ -12,13 +13,14 @@ use Maslosoft\ManganTest\Models\Indexes\ModelWithCompoundI18NIndexShortNotation;
 use Maslosoft\ManganTest\Models\Indexes\ModelWithHashedIndex;
 use Maslosoft\ManganTest\Models\Indexes\ModelWithI18NIndex;
 use Maslosoft\ManganTest\Models\Indexes\ModelWithSimpleIndex;
+use UnitTester;
 use function json_decode;
 use const JSON_OBJECT_AS_ARRAY;
 
-class IndexManagerTest extends \Codeception\Test\Unit
+class IndexManagerTest extends Unit
 {
     /**
-     * @var \UnitTester
+     * @var UnitTester
      */
     protected $tester;
 
@@ -32,7 +34,7 @@ class IndexManagerTest extends \Codeception\Test\Unit
 
     // tests
 
-	public function testStoragePath()
+	public function testStoragePath(): void
 	{
 		$model = new ModelWithSimpleIndex;
 		$path = IndexManager::fly()->getStoragePath($model, get_class($model));
@@ -40,7 +42,7 @@ class IndexManagerTest extends \Codeception\Test\Unit
 		$this->assertNotEmpty($path, 'That path exists');
 	}
 
-	public function testSimpleIndexCreation()
+	public function testSimpleIndexCreation(): void
     {
     	$model = new ModelWithSimpleIndex;
     	$success = IndexManager::fly()->create($model);
@@ -51,7 +53,7 @@ class IndexManagerTest extends \Codeception\Test\Unit
     	$this->assertTrue($success, 'That index was created');
     }
 
-	public function testI18NIndexCreation()
+	public function testI18NIndexCreation(): void
 	{
 		$model = new ModelWithI18NIndex;
 		$model->setLanguages(['en', 'pl', 'es']);
@@ -69,7 +71,7 @@ class IndexManagerTest extends \Codeception\Test\Unit
 		$this->assertTrue($success, 'That index was created');
 	}
 
-	public function testCompoundI18NIndexCreation()
+	public function testCompoundI18NIndexCreation(): void
 	{
 		$model = new ModelWithCompoundI18NIndex;
 		$model->setLanguages(['en', 'pl', 'es']);
@@ -88,7 +90,7 @@ class IndexManagerTest extends \Codeception\Test\Unit
 		$this->assertTrue($success, 'That index was created');
 	}
 
-	public function testCompoundI18NShortNotationIndexCreation()
+	public function testCompoundI18NShortNotationIndexCreation(): void
 	{
 		$model = new ModelWithCompoundI18NIndexShortNotation;
 		$model->setLanguages(['en', 'pl', 'es']);
@@ -109,7 +111,7 @@ class IndexManagerTest extends \Codeception\Test\Unit
 		$this->assertTrue($success, 'That index was created');
 	}
 
-	public function testHashedIndexCreation()
+	public function testHashedIndexCreation(): void
 	{
 		$model = new ModelWithHashedIndex;
 		$model->setLanguages(['en', 'pl', 'es']);
@@ -122,7 +124,7 @@ class IndexManagerTest extends \Codeception\Test\Unit
 		$this->assertTrue($success, 'That index was created');
 	}
 
-	public function test2dSphereIndexCreation()
+	public function test2dSphereIndexCreation(): void
 	{
 		$model = new ModelWith2dSphere;
 		$success = IndexManager::fly()->create($model);
@@ -134,7 +136,7 @@ class IndexManagerTest extends \Codeception\Test\Unit
 		$this->assertTrue($success, 'That index was created');
 	}
 
-	public function test2dSphereIndexExtentedNotationCreation()
+	public function test2dSphereIndexExtentedNotationCreation(): void
 	{
 		$model = new ModelWith2dSphereExtendedNotation;
 		$success = IndexManager::fly()->create($model);
@@ -146,7 +148,7 @@ class IndexManagerTest extends \Codeception\Test\Unit
 		$this->assertTrue($success, 'That index was created');
 	}
 
-	public function testSimpleIndexAutoCreation()
+	public function testSimpleIndexAutoCreation(): void
 	{
 		$model = new ModelWithSimpleIndex;
 		(new Finder($model))->find();
@@ -160,7 +162,7 @@ class IndexManagerTest extends \Codeception\Test\Unit
 	{
 		$cmd = new Command($model);
 		$stats = $cmd->collStats(CollectionNamer::nameCollection($model));
-		$this->markTestIncomplete("The returned results is an instance of Cursor, not array. And does not contain any data at all?");
+//		$this->markTestIncomplete("The returned results is an instance of Cursor, not array. And does not contain any data at all?");
 		$this->assertArrayHasKey('indexDetails', $stats, 'There are indices in ' . get_class($model));
 		$idxs = $stats['indexDetails'];
 		$formatted = [];
