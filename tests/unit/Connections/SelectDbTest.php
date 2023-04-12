d3341878b3afd9376c2f0b52399f5294567f5e30
Peter Maselkowski <office@maslosoft.com>
2023-04-12 13:54:35 +0200

    * Tests

--- tests/unit/Connections/SelectDbTest.php
+++ tests/unit/Connections/SelectDbTest.php
@@ -3,12 +3,11 @@
 namespace Connections;
 
 use Codeception\Test\Unit;
+use Maslosoft\Mangan\Command;
 use Maslosoft\Mangan\EntityManager;
 use Maslosoft\Mangan\Finder;
 use Maslosoft\Mangan\Mangan;
 use Maslosoft\ManganTest\Models\ActiveDocument\DocumentBaseAttributes;
-use Maslosoft\ManganTest\Models\ModelWithLabel;
-use MongoDB;
 use MongoDB\BSON\ObjectId as MongoId;
 use UnitTester;
 
@@ -31,10 +30,13 @@ class SelectDbTest extends Unit
 	}
 
 	// tests
-	public function testIfWillAllowSelectingDB()
+	public function testIfWillAllowSelectingDB(): void
 	{
-		// Default
+		// Default connection settings
 		$mangan = Mangan::fly();
+
+		$dbStats = (new Command(null, $mangan))->dbStats();
+		$this->assertSame(ManganFirstDbName, $dbStats['db']);
 
 		$mangan->selectDB(ManganForthDbName);
 
@@ -45,16 +47,23 @@ class SelectDbTest extends Unit
 		$saved = $em->save();
 
 		$this->assertTrue($saved, 'Model was saved');
+		$finder = new Finder($model, null, $mangan);
+		$count = $finder->count();
+		$this->assertSame(1, $count, 'That db contains model that was saved to first database');
 
-		$collections = $mangan->getDbInstance()->listCollections();
+
+
+		$collections = iterator_to_array($mangan->getDbInstance()->listCollections());
+		$dbStats = (new Command(null, $mangan))->dbStats();
+		$this->assertSame(ManganForthDbName, $dbStats['db']);
 		codecept_debug($collections);
 
 		$mangan2 = Mangan::fly('four');
-		$collections = $mangan2->getDbInstance()->listCollections();
+		$collections = iterator_to_array($mangan2->getDbInstance()->listCollections());
 		codecept_debug($collections);
 		$finder = new Finder($model, null, $mangan2);
 		$count = $finder->count();
-		$this->assertSame(1, $count, 'That model was saved to other database');
+		$this->assertSame(1, $count, 'That db contains model that was saved to other database');
 	}
 
 }
