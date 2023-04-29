603b89efa6eb8c1bc044460087f8cec616efe56a
Peter Maselkowski <office@maslosoft.com>
2023-04-29 10:36:17 +0200

    * Clarified test description

--- tests/unit/Issues/I88Test.php
+++ tests/unit/Issues/I88Test.php
@@ -3,9 +3,6 @@
 use function codecept_debug;
 use Codeception\Test\Unit;
 use Maslosoft\Mangan\Exceptions\StructureException;
-use Maslosoft\Mangan\Finder;
-use Maslosoft\Mangan\Helpers\Debug\StructureChecker;
-use Maslosoft\Mangan\Transformers\RawArray;
 use Maslosoft\ManganTest\Models\Issues\Model88;
 use Maslosoft\ManganTest\Models\Issues\Model88Embedded;
 use UnitTester;
@@ -18,7 +15,14 @@ class I88Test extends Unit
     protected $tester;
 
     // tests
-    public function testAsserting()
+
+	/**
+	 * When trying to save model with field containing document which is not marked with annotations as document, the mongo adapter will fall
+	 * into infinite loop or will exhaust memory, whatever will come first.
+	 * @link https://github.com/Maslosoft/Mangan/issues/88
+	 * @return void
+	 */
+    public function testAssertingEmbeddedAnnotation(): void
     {
     	$model = new Model88;
     	$model->stats = new Model88Embedded;
